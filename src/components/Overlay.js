22138002f263430c6c5235aac49558cd1da98c73
Busch, Mathew <Mathew.Busch@wunderman.com>
2019-06-08 20:49:07 +1000

    test netlify alive take 2

---
+++ src/components/Overlay.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Overlay = styleMedia.div`
+    width: 100%;
+    height: 100%;
+    background: #26292e;
+    opacity: 0.7;
+    zIndex: 0;
+`;
+
+// const HeroText = styleMedia.div`
+// position: absolute:
+// left: 50%;
+// top: 50%;
+// margin: -150px 0 0 -585px;
+// width: 1170px;
+// height: 250px;
+// text-align: center;
+// zIndex: 2;
+// `;
+
+render(
+    // return (
+    < Overlay >
+        {/* <HeroText>
+            <h1 className="main-hero-text">
+                Hi, i'm Mat a UX, UI Designer who loves to create,</h1>
+            <h1 className="typewrite" data-period="2000" data-type="['Hand crafted websites', 'Product Prototypes', User Interfaces, 'VR Experiences', 'High fidelity mockups', 'Information Visualisatons']">...</h1>
+        </HeroText> */}
+
+    </Overlay >
+);
+
+
+export default Overlay
